95bec6abd3e1d8277c55686b35ee90fde6da7895
mandarpatil0331 <65004527+mandarpatil0331@users.noreply.github.com>
2020-05-08 15:28:16 +0530

    codeblocks
    first file contain sizeof program
    second file contain product of two number

---
+++ sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"size of char:"<<sizeof(char)<<"\n";
+    cout<<"size of int:"<<sizeof(int)<<"\n";
+    cout<<"size of float:"<<sizeof(float)<<"\n";
+    cout<<"size of double:"<<sizeof(double);
+    return 0;
+}
